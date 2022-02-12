865ab4dc6a653e2a8a8e05d5e0258ce5a6b5f09f
korney3 <koren.iz3x@yandex.ru>
2022-02-12 23:38:22 +0300

    Add morgan fingerprint draft baseline

--- src/baselines/morgan_fingerprints.py
+++ src/baselines/morgan_fingerprints.py
@@ -2,7 +2,7 @@ from rdkit import Chem
 from rdkit.Chem import AllChem
 
 
-def get_morgan_fingerprint(smiles: str, radius=3, nBits=1024):
+def get_morgan_fingerprint(smiles: str, radius=2, nBits=2048):
     mol = Chem.MolFromSmiles(smiles)
     morgan_fp = AllChem.GetMorganFingerprintAsBitVect(mol, radius=radius, nBits=nBits)
     return (list(morgan_fp))
--- src/baselines/run_baseline.py
+++ src/baselines/run_baseline.py
@@ -19,47 +19,36 @@ def main():
     train_morgan_fp = list(map(lambda x: get_morgan_fingerprint(x), smiles_train))
     test_morgan_fp = list(map(lambda x: get_morgan_fingerprint(x), smiles_test))
 
-    xgboost_parameters = [
-        {"kernel": ["rbf"], "gamma": [1e-3, 1e-4], "C": [1, 10, 100, 1000]},
-    ]
 
     params = {
         # 'min_child_weight': [1, 5, 10],
         # 'gamma': [0.5, 1, 1.5, 2, 5],
         # 'subsample': [0.6, 0.8, 1.0],
         # 'colsample_bytree': [0.6, 0.8, 1.0],
-        'max_depth': [10, 100, 200, 300, 500, 600],
-        'n_estimators': [50, 100, 200, 300, 400]
+        'max_depth': [50, 100, 200, 300, 500, 600, 900],
+        'n_estimators': [300, 400, 500, 1000]
     }
-    params = {
-        # 'min_child_weight': [1, 5, 10],
-        # 'gamma': [0.5, 1, 1.5, 2, 5],
-        # 'subsample': [0.6, 0.8, 1.0],
-        # 'colsample_bytree': [0.6, 0.8, 1.0],
-        'max_depth': [100],
-        'n_estimators': [400]
-    }
-    xgb = XGBClassifier(learning_rate=0.02, n_estimators=600,  nthread=1)
-    folds = 6
+    xgb = XGBClassifier(learning_rate=0.02, n_estimators=600, nthread=1)
+    folds = 4
 
     skf = StratifiedKFold(n_splits=folds, shuffle=True, random_state=1001)
 
     grid_search = GridSearchCV(xgb, param_grid=params, scoring='f1', n_jobs=4,
-                               cv=skf.split(train_morgan_fp, y_train), verbose=1000)
+                               cv=skf.split(train_morgan_fp, y_train), verbose=3)
     print('\n Start Grid Search')
     grid_search.fit(train_morgan_fp, y_train)
     print('\n All results:')
     print(grid_search.cv_results_)
     print('\n Best estimator:')
     print(grid_search.best_estimator_)
-    print('\n Best  for %d-fold search with:' % (folds))
+    print('\n Best normalized score for %d-fold search with:' % (folds))
     print(grid_search.best_score_)
     print('\n Best hyperparameters:')
     print(grid_search.best_params_)
 
     test_predictions = grid_search.best_estimator_.predict(test_morgan_fp)
     test_predictions_df = save_prediction(test_data.data[SMILES_COLUMN], test_predictions,
-                                          "morgan_fp_test_submission.csv")
+                                          "morgan_fp_2_2048_test_submission.csv")
     return
 
 
