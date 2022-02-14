5fde86e7b8ce6a19f88dd41b08c436062fd98d97
sovadim <vadik.sofin@yandex.ru>
2022-02-14 21:48:16 +0300

    Create predictions dir if not exists

--- src/data/save_predictions.py
+++ src/data/save_predictions.py
@@ -11,5 +11,9 @@ def save_prediction(test_smiles: List[str], predictions: List[int], filename: st
     test_submission = pd.DataFrame(columns=[SMILES_COLUMN, TARGET_COLUMN])
     test_submission[SMILES_COLUMN] = test_smiles
     test_submission[TARGET_COLUMN] = predictions
+
+    if not os.path.exists(PREDICTIONS_PATH):
+        os.mkdir(PREDICTIONS_PATH)
+
     test_submission.to_csv(os.path.join(predictions_dir, filename))
     return test_submission
